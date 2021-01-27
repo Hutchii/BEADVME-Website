2e043d89b90931b3030d1b94932ceaf4432ced35
Hutchii <ravodola@gmail.com>
2021-01-27 17:30:10 +0100

    JS Changes

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,7 @@
 const pr = document.getElementsByClassName("pr");
 const tr = document.getElementsByClassName("tr");
 
-function portfolio(index) {
+function portfolioChange(index) {
   pr[index].addEventListener("mouseover", () => {
     for (let i = 0; i < pr.length; i++) {
       if (i === index) {
@@ -15,18 +15,5 @@ function portfolio(index) {
   });
 }
 for (let i = 0; i < pr.length; i++) {
-  portfolio(i);
+  portfolioChange(i);
 }
-// let index = 0;
-
-// for (let i = 0; i < pr.length; i++) {
-//   pr[i].addEventListener("mouseover", () => {
-//     if (i === 1) {
-//       pr[i].classList.add("color-change");
-//       tr[i].classList.remove("transform-portfolio");
-//     } else {
-//       pr[i].classList.remove("color-change");
-//       tr[i].classList.add("transform-portfolio");
-//     }
-//   });
-// }
