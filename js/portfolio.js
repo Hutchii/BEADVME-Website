0d30d611e1be473a8dacccd3b11b97da9135961d
Hutchii <ravodola@gmail.com>
2021-01-27 15:24:52 +0100

    JS Test

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,8 @@
 const pr = document.getElementsByClassName("pr");
 const tr = document.getElementsByClassName("tr");
 
-function portfolio(click, index) {
-  click.addEventListener("mouseover", () => {
+function portfolio(index) {
+  pr[index].addEventListener("mouseover", () => {
     for (let i = 0; i < pr.length; i++) {
       if (i === index) {
         pr[i].classList.add("color-change");
@@ -15,5 +15,18 @@ function portfolio(click, index) {
   });
 }
 for (let i = 0; i < pr.length; i++) {
-  portfolio(pr[i], i);
+  portfolio(i);
 }
+// let index = 0;
+
+// for (let i = 0; i < pr.length; i++) {
+//   pr[i].addEventListener("mouseover", () => {
+//     if (i === 1) {
+//       pr[i].classList.add("color-change");
+//       tr[i].classList.remove("transform-portfolio");
+//     } else {
+//       pr[i].classList.remove("color-change");
+//       tr[i].classList.add("transform-portfolio");
+//     }
+//   });
+// }
